a7137664d6c3112caf3b279aaffcba9dbc30ca60
john <kom.huy@gmail.com>
2015-06-02 18:18:21 +0700

    add new function for edit module show info

--- admin/js/ng/app/modules/controller/modules_edit_ctrl.js
+++ admin/js/ng/app/modules/controller/modules_edit_ctrl.js
@@ -5,5 +5,17 @@ app.controller(
 	, 'Services'
 	, function ($scope, Factory, Services){
 		
+		$scope.module_edit = [];
+		$scope.edit = function(params){
+			var path = $('#path').attr('class');
+			var dataParse = {
+				class_name: params.code, 
+				path: path
+			};
+			Factory.editModules(dataParse).success(function(data){
+				$scope.module_edit = data;
+			});
+		};
+		
 	}
 ]);
