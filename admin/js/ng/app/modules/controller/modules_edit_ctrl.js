207bb0f911deaeee91f60de966b5272cff85b39a
john <kom.huy@gmail.com>
2015-06-06 12:56:51 +0700

    add jquery append html element when edit module

--- admin/js/ng/app/modules/controller/modules_edit_ctrl.js
+++ admin/js/ng/app/modules/controller/modules_edit_ctrl.js
@@ -6,14 +6,16 @@ app.controller(
 	, function ($scope, Factory, Services){
 		
 		$scope.module_edit = [];
-		$scope.edit = function(params){
+		$scope.edit = function(params, header){
+			$scope.header = header;
+			$('#element').empty();
 			var path = $('#path').attr('class');
 			var dataParse = {
 				class_name: params.code, 
 				path: path
 			};
 			Factory.editModules(dataParse).success(function(data){
-				$scope.module_edit = data;
+				$('#element').append(data);
 			});
 		};
 		
