4dc0f55b51ab626fa1913419c645af33828907c5
john <kom.huy@gmail.com>
2015-06-09 13:19:59 +0700

    remove no need function from service

--- admin/js/ng/app/modules/services/services.js
+++ admin/js/ng/app/modules/services/services.js
@@ -1,20 +1,6 @@
 app.service("Services", 
 	function() {
 		
-        this.removeObject = function(array, id){
-        	
-        	var index = -1;
-			_.each(array, function(data, idx) {
-				if( typeof(data) != 'undefined' ){
-					if ( data.title == id ){
-						index = idx;
-					}
-				}
-			});
-			array.splice(index, 1);
-			
-        };
-        
         this.alertMessage = function(attr){
         	$(attr).show();
 			setTimeout(function(){
