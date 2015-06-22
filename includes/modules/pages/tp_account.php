88c2ffdc68110951324ce856b0408c92249ea9ad
john <kom.huy@gmail.com>
2015-06-22 18:57:38 +0700

    change style account page template

--- includes/modules/pages/tp_account.php
+++ includes/modules/pages/tp_account.php
@@ -16,30 +16,56 @@
     function prepare() {
       global $oscTemplate;
 
-      $oscTemplate->_data[$this->group] = array('account' => array('title' => MY_ACCOUNT_TITLE,
-                                                                   'sort_order' => 10,
-                                                                   'links' => array('edit' => array('title' => MY_ACCOUNT_INFORMATION,
-                                                                                                    'link' => tep_href_link(FILENAME_ACCOUNT_EDIT, '', 'SSL'),
-                                                                                                    'icon' => 'glyphicon glyphicon-user'),
-                                                                                    'address_book' => array('title' => MY_ACCOUNT_ADDRESS_BOOK,
-                                                                                                            'link' => tep_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'),
-                                                                                                            'icon' => 'glyphicon glyphicon-home'),
-                                                                                    'password' => array('title' => MY_ACCOUNT_PASSWORD,
-                                                                                                        'link' => tep_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'),
-                                                                                                        'icon' => 'glyphicon glyphicon-cog'))),
-                                                'orders' => array('title' => MY_ORDERS_TITLE,
-                                                                  'sort_order' => 20,
-                                                                  'links' => array('history' => array('title' => MY_ORDERS_VIEW,
-                                                                                                      'link' => tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL'),
-                                                                                                      'icon' => 'glyphicon glyphicon-shopping-cart'))),
-                                                'notifications' => array('title' => EMAIL_NOTIFICATIONS_TITLE,
-                                                                         'sort_order' => 30,
-                                                                         'links' => array('newsletters' => array('title' => EMAIL_NOTIFICATIONS_NEWSLETTERS,
-                                                                                                                 'link' => tep_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL'),
-                                                                                                                 'icon' => 'glyphicon glyphicon-envelope'),
-                                                                                          'products' => array('title' => EMAIL_NOTIFICATIONS_PRODUCTS,
-                                                                                                              'link' => tep_href_link(FILENAME_ACCOUNT_NOTIFICATIONS, '', 'SSL'),
-                                                                                                              'icon' => 'glyphicon glyphicon-send'))));
+      $oscTemplate->_data[$this->group] = array(
+      		'account' => array(
+      			'title' => MY_ACCOUNT_TITLE,
+      			'sort_order' => 10,
+      			'links' => array(
+      				'edit' => array(
+      					'title' => MY_ACCOUNT_INFORMATION,
+      					'link' => tep_href_link(FILENAME_ACCOUNT_EDIT, '', 'SSL'),
+      					'icon' => 'glyphicon glyphicon-user'
+      				),
+      				'address_book' => array(
+      					'title' => MY_ACCOUNT_ADDRESS_BOOK,
+      					'link' => tep_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'),
+      					'icon' => 'glyphicon glyphicon-home'
+      				),
+      				'password' => array(
+      					'title' => MY_ACCOUNT_PASSWORD,
+      					'link' => tep_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'),
+      					'icon' => 'glyphicon glyphicon-cog'
+      				)
+      			)
+      		),
+      		'orders' => array(
+      			'title' => MY_ORDERS_TITLE,
+      			'sort_order' => 20,
+      			'links' => array(
+      				'history' => array(
+      					'title' => MY_ORDERS_VIEW,
+      					'link' => tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL'),
+      					'icon' => 'glyphicon glyphicon-shopping-cart'
+      				)
+      			)
+      		),
+      		'notifications' => array(
+      			'title' => EMAIL_NOTIFICATIONS_TITLE,
+      			'sort_order' => 30,
+      			'links' => array(
+      				'newsletters' => array(
+      					'title' => EMAIL_NOTIFICATIONS_NEWSLETTERS,
+      					'link' => tep_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL'),
+      					'icon' => 'glyphicon glyphicon-envelope'
+      				),
+      				'products' => array(
+      					'title' => EMAIL_NOTIFICATIONS_PRODUCTS,
+	      				'link' => tep_href_link(FILENAME_ACCOUNT_NOTIFICATIONS, '', 'SSL'),
+	      				'icon' => 'glyphicon glyphicon-send'
+      				)
+      			)
+      		)
+      	);
     }
 
     function build() {
@@ -53,8 +79,9 @@
       $output = '<div class="col-sm-12">';
 
       foreach ( $oscTemplate->_data[$this->group] as $group ) {
-        $output .= '<h2>' . $group['title'] . '</h2>' .
-                   '<div class="contentText">' .
+      	$output .= '<div class="panel panel-default">';
+        $output .= '<div class="panel-heading">' . $group['title'] . '</div>' .
+                   '<div class="panel-body">' .
                    '  <ul class="accountLinkList">';
 
         foreach ( $group['links'] as $entry ) {
@@ -68,7 +95,7 @@
         }
 
         $output .= '  </ul>' .
-                   '</div>';
+                   '</div></div>';
       }
 
       $output .= '</div>';
