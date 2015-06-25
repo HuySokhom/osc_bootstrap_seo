54c33b21126e072caa2542b2ccdf8ce5e763d8bf
john <kom.huy@gmail.com>
2015-06-25 13:56:32 +0700

    add new method for update customers

--- includes/classes/autoload/RestApiSessionCustomer.php
+++ includes/classes/autoload/RestApiSessionCustomer.php
@@ -7,23 +7,39 @@ use
 
 class RestApiSessionCustomer extends RestApi {
 
-	public function get(){
-		
+	public function get(){		
 		$col = new CustomerCol();
-		
+		$customerId = $this->getOwner()->getId();		
+		if ( ! $customerId ) {
+			throw new \Exception(
+				"403: Access Denied",
+				403
+			);
+		}else{			
+			$col->filterById($customerId);
+			$this->applySortBy($col, $params);
+			return $this->getReturn($col, $params);			
+		}
+	}
+	
+	public function put($params){
+		$cols = new CustomerCol();
 		$customerId = $this->getOwner()->getId();
-		
 		if ( ! $customerId ) {
 			throw new \Exception(
 				"403: Access Denied",
 				403
 			);
 		}else{
-			
-			$col->filterById($customerId);
-			$this->applySortBy($col, $params);
-			return $this->getReturn($col, $params);
-			
+			$cols->filterById($customerId);
+			if( $cols->getTotalCount() > 0 ){
+				$cols->populate();
+				$col = $cols->getFirstElement();
+				$col->setId($customerId);
+				$col->setProperties($params['PUT']);				
+				$col->update();
+			}
 		}
 	}
+	
 }
