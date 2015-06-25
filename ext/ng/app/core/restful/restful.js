179d672be09feed8bb3c24dd44792964b30b78dd
john <kom.huy@gmail.com>
2015-06-25 13:30:56 +0700

    new core file for restful

---
+++ ext/ng/app/core/restful/restful.js
@@ -0,0 +1,42 @@
+app.factory("Restful", [
+ 	'$http', 
+ 	function ($http) {
+        
+        var obj = {};
+        // start get 
+        obj.get = function(url, params) {
+            return $http({
+        		url: url,
+    			method: 'GET',
+    			params: params
+            });
+        };
+        
+        obj.put = function(url, params){
+        	return $http({
+        		url: url,
+    			method: 'PUT',
+    			data: params
+        	});
+        };
+        
+        obj.patch = function(url, params){
+        	return $http({
+        		url: url,
+    			method: 'PATCH',
+    			data: params
+        	});
+        };
+        
+        obj.delete = function(url, params){
+        	return $http({
+        		url: url,
+    			method: 'DELETE',
+    			data: params
+        	});
+        };
+        
+        return obj;
+        
+ 	}
+]);
