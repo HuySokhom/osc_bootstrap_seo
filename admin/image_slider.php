1e9c2e759bc85d242cda68f68a611bb92045bc31
huysokhom <kom.huy@gmail.com>
2015-12-31 12:26:19 +0700

    show no image if image silder doesn't have

--- admin/image_slider.php
+++ admin/image_slider.php
@@ -10,22 +10,9 @@ require(DIR_WS_INCLUDES . 'template_top.php');
 <link rel="stylesheet" type="text/css" href="css/image_slider.css">
 <body data-ng-app="main">
 <h4>Image Slider</h4>
-<div class="col-md-12">
-    <div class="panel with-nav-tabs panel-default">
-        <div class="panel-heading">
-            <ul class="nav nav-tabs">
-                <li data-ui-sref-active="active">
-                    <a data-ui-sref="/image_slider" data-toggle="tab">
-                        Image Slider
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="panel-body">
-            <div class="tab-content">
-                <div data-ui-view=""></div>
-            </div>
-        </div>
+<div class="panel-body">
+    <div class="tab-content">
+        <div data-ui-view=""></div>
     </div>
 </div>
 <!-- lib -->
