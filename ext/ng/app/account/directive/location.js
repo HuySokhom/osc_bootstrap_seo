ae9982db200b22498c4fee14c87c1b130af7fa35
huysokhom <kom.huy@gmail.com>
2016-01-02 16:32:28 +0700

    fix location directive

--- ext/ng/app/account/directive/location.js
+++ ext/ng/app/account/directive/location.js
@@ -1,13 +1,15 @@
 app.directive('locationAccount',[
-	'Restful'
-	, function(Restful){
-    return {
-        link: function($scope, $element, $attrs){
-        	var urlLocation = 'api/Location';
-			Restful.get(urlLocation).success(function(data){
-				$scope.location = data;
-			});
-        },
-        templateUrl: 'ext/ng/app/account/partials/location.html'
-    };
-}]);
+	'Restful',
+	function(Restful){
+		return {
+			restrict: 'EA',
+			templateUrl : 'ext/ng/app/account/partials/location.html',
+			link: function ($scope, element, $attr) {
+				var urlLocation = 'api/Location';
+				Restful.get(urlLocation).success(function(data){
+					$scope.location = data;
+				});
+			}
+		};
+	}
+]);
