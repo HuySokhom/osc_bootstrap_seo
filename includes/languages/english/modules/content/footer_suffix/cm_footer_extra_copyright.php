d9655954efe0610753e6227076ccab2ae2063ad9
HuySokhom <kom.huy@gmail.com>
2016-03-25 15:58:17 +0700

    update style css

--- includes/languages/english/modules/content/footer_suffix/cm_footer_extra_copyright.php
+++ includes/languages/english/modules/content/footer_suffix/cm_footer_extra_copyright.php
@@ -13,5 +13,9 @@
   define('MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_TITLE', 'Copyright Details');
   define('MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_DESCRIPTION', 'Adds a Copyright Block to the Extra Footer Area of your site');
   
-  define('FOOTER_TEXT_BODY', '<p>Copyright &copy; ' . date('Y') . ' <a href="' . tep_href_link(FILENAME_DEFAULT) . '">' . STORE_NAME . '</a> Powered by <a href="http://www.oscommerce.com" target="_blank">osCommerce</a></p>');
+  define('FOOTER_TEXT_BODY', '<p>Copyright &copy; ' . date('Y')
+      . ' <a href="' . tep_href_link(FILENAME_DEFAULT) . '" style="color: #f20420;">'
+      . STORE_NAME . '</a>  | All Rights Reserved
+      | The UTIP shield is the only <a href="http://myUTIP.com" style="color: #f20420;">Vaginal Hygiene Product</a>
+      | <a href="http://CleanVaginaMovement.com" style="color: #f20420;"> Helping Women to End Most Vaginal Infections </a></p>');
   
--- includes/modules/content/footer_suffix/templates/copyright.php
+++ includes/modules/content/footer_suffix/templates/copyright.php
@@ -1,3 +1,3 @@
-<div class="col-sm-<?php echo $content_width; ?> text-center-xs">
+<div class="col-md-12 text-center" style="background-color: #40e566;color: #0f0f0f;">
   <?php echo FOOTER_TEXT_BODY; ?>
 </div>
